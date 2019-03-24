317607c2028801e9181f471399a3aac53ef0da5c
Ritu Raj Basant <riturajbasant86@gmail.com>
2019-03-24 13:05:34 +0530

    commit testing

---
+++ js/lib/enumerate.py
@@ -0,0 +1,15 @@
+# enumerate is a predefined function and it will return index as well as values
+# we will pass a sequence as  a parameter in enumberate
+
+
+'''
+for i, j in enumerate (range (4,30,2)):
+    print(i,j)
+'''
+
+#zip
+
+for i, j in zip(range(4,10,1), range(1,6)):
+    
+
+    #This is test file and checking git
